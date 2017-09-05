be29b705da59b64d4362547bd83970cc79158eda
johncouldbe <johndouglasb@gmail.com>
2017-09-05 17:42:06 -0400

    Fixed an enzyme test after changing the RightSideBar component to being stateful

--- client/src/components/right-sidebar/RightSidebar.test.js
+++ client/src/components/right-sidebar/RightSidebar.test.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import store from '../../store'
+
 import RightSidebar from './RightSidebar'
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<RightSidebar />, div)
+  ReactDOM.render(
+    <Provider store={store}>
+      <RightSidebar />
+    </ Provider>
+    , div)
 })
