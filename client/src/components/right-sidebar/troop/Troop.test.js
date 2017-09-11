d928b88ca81a2b7832e50ab0684ec8c10ecf1bca
johncouldbe <johndouglasb@gmail.com>
2017-09-11 15:30:33 -0400

    Correcting a test error for Troop  component

--- client/src/components/right-sidebar/troop/Troop.test.js
+++ client/src/components/right-sidebar/troop/Troop.test.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Troop from './Troop'
+import { Provider } from 'react-redux'
+import store from '../../../store'
 
 it('Renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<Troop />, div)
+  ReactDOM.render(
+    <Provider store={store}>
+      <Troop />
+    </ Provider>, div)
 })
