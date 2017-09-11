f5d55aae864ab75770cf3207c5d2e29469436f50
johncouldbe <johndouglasb@gmail.com>
2017-09-11 15:38:57 -0400

    Testing environment variables in netlify

--- client/src/components/recruit-modal/RecruitModal.test.js
+++ client/src/components/recruit-modal/RecruitModal.test.js
@@ -1,13 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import RecruitModal from './RecruitModal'
-import { Provider } from 'react-redux'
-import store from '../../store'
 
 it('Renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(
-    <Provider store={store}>
-      <RecruitModal />
-    </ Provider>, div)
+  ReactDOM.render(<RecruitModal />, div)
 })
