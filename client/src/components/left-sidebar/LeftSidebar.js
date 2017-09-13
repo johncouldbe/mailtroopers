fd1fe48dff98ae0e536c14fdfe3f3331234086c2
johncouldbe <johndouglasb@gmail.com>
2017-09-13 13:21:59 -0400

    Updating config.js file to heroku app on production

--- client/src/components/left-sidebar/LeftSidebar.js
+++ client/src/components/left-sidebar/LeftSidebar.js
@@ -7,9 +7,17 @@ function LeftSidebar (props) {
       <div className="left-sidebar">
         <div className="review-header">
           <div className="h2 grey-text center-text">Reviews</div>
-          <div className="faded-border"></div>
         </div>
-
+        <div className="email-list-container">
+          <div className="email-file">
+            <div className="h4 grey-text comment-date">ARA September Education Seminar</div>
+            <div className="p grey-text comment-comment">09/12/17</div>
+          </div>
+          <div className="email-file">
+            <div className="h4 grey-text comment-date">ARA September Education Seminar</div>
+            <div className="p grey-text comment-comment">09/12/17</div>
+          </div>
+        </div>
 
       </div>
   )
