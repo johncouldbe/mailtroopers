5d838cdd52358802a7f365548c459bb43346700a
johncouldbe <johndouglasb@gmail.com>
2017-11-05 11:46:20 -0500

    Updated Travis testing

--- client/src/components/app/App.test.js
+++ client/src/components/app/App.test.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
-import Enzyme, {shallow} from 'enzyme'
-import Adapter from 'enzyme-adapter-react-15'
+import {shallow} from 'enzyme'
 
 import {App}  from './App'
-
-
-Enzyme.configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   shallow(<App />)
